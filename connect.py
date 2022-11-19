622ec6e50d7e16aa03e6657172864411dc65800a
VeryOldpApY <dalix.playgamer@gmail.com>
2022-11-19 13:08:10 +0100

    Update pApY

--- connect.py
+++ connect.py
@@ -1,43 +1,68 @@
 import os
 import paramiko
 import json
+import stat
 
 
-def connect(dns, port, user, password):
+def connect(host, port, user, password):
 	connecting = paramiko.SSHClient()
 	connecting.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 	# Usage env variable
 	if password == "":
-		connecting.connect(dns, port, user)
+		connecting.connect(host, port, user)
 		print("no mdp")
 	else:
-		connecting.connect(dns, port, user, password)
+		connecting.connect(host, port, user, password)
 		print("mdp")
 	print("connected")
 	return connecting
 
+
 # Remove the error the make the remote code execution possible
-def verify_folder(link, remote_path):
-	_, stdout, _ = link.exec_command("cd " + remote_path)
-	if stdout.channel.recv_exit_status() != 0:
-		link.exec_command("mkdir " + remote_path)
-		print("create folder : " + remote_path)
-
-
-def send_folder(link, local_path, remote_path):
-	sftp = link.open_sftp()
-	for i in os.listdir(local_path):
-		if not os.path.isdir(local_path + "/" + i):
-			sftp.put(local_path + i, remote_path + i)
-			print("send file : " + remote_path + i)
+def verifyFolder(path, link=None):
+	if link is None:
+		if not os.path.isdir(path):
+			os.mkdir(path)
+			print("create local folder : " + path)
 		else:
-			verify_folder(link, remote_path + i)
-			send_folder(link, local_path + i + "/", remote_path + i + "/")
+			print("folder local already exist : " + path)
+	else:
+		stdin, stdout, stderr = link.exec_command("cd " + path)
+		if stdout.channel.recv_exit_status() != 0:
+			link.exec_command("mkdir " + path)
+			print("create remote folder : " + path)
+		else:
+			print("folder remote already exist : " + path)
+
+
+def sendFolder(link, localPath, remotePath):
+	sftp = link.open_sftp()
+	for i in os.listdir(localPath):
+		if not os.path.isdir(localPath + i):
+			sftp.put(localPath + i, remotePath + i)
+			print("send file : " + remotePath + i)
+		else:
+			verifyFolder(remotePath + i, link)
+			sendFolder(link, localPath + i + "/", remotePath + i + "/")
+	sftp.close()
+
+
+def getFolder(link, localPath, remotePath):
+	sftp = link.open_sftp()
+	for i in sftp.listdir(remotePath):
+		if not sftp.chdir(remotePath + i):
+			sftp.get(remotePath + i, localPath + i)
+			print("get file : " + localPath + i)
+		else:
+			print(localPath + i)
+			verifyFolder(localPath + i)
+			getFolder(link, localPath + i + "/", remotePath + i + "/")
 	sftp.close()
 
 
 config = json.load(open("config.json"))
-ssh = connect(config["dns"], config["port"], config["user"], config["password"])
-verify_folder(ssh, config["remotePath"])
-send_folder(ssh, config["localPath"], config["remotePath"])
+ssh = connect(config["host"], config["port"], config["user"], config["password"])
+verifyFolder(config["remotePath"], ssh)
+# verifyFolder(config["localPath"])
+# getFolder(ssh, config["localPath"], config["remotePath"])
