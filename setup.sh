13957cab8e4825144901276cf9733be1cd5954c0
maleyy <wapzif@gmail.com>
2022-11-19 16:23:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,26 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+if [ ! -d "./.as_been_installed" ]
+  then echo "1" > .as_been_installed
+fi
+
+# Read the file to know if the script has been run before 
+if [ "$(cat .as_been_installed)" -eq 1 ]
+  then echo "The script has already been run"
+
+  # Ask to overwrite the previous installation
+  read -p "Do you want to overwrite the previous installation? [y/n] " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+    then echo "0" > .as_been_installed
+  else
+    exit
+  fi
+fi
+
+echo "1" > .as_been_installed
+
 # Install python3, pip3 and all the libraries in requirements.txt
 sudo apt-get update
 sudo apt-get install python3 python3-pip
@@ -19,13 +39,17 @@ sudo pip3 install -r requirements.txt
 echo "Enter the path to the directory where the files will be stored:"
 read path
 
-# Ask the user to enter password to connect in ssh
-echo "Enter the password to connect in ssh:"
-read password
+# Ask the user name
+echo "Enter the user name:"
+read user
 
 # Ask the user to enter the ip address of the server
 echo "Enter the ip address of the server:"
 read ip
+
+# Ask the user to enter password to connect in ssh
+echo "Enter the password to connect in ssh:"
+read password
 
 # Create the directory if it doesn't exist
 if [ ! -d "$path" ]
@@ -39,6 +63,6 @@ read time
 # Create the crontab
 crontab -l > mycron
 echo "0 * * * * cd $path" >> mycron
-echo "*/$time * * * * sshpass -p $password scp * user@$ip:$path" >> mycron
+echo "*/$time * * * * sshpass -p $password scp * $user@$ip:$path" >> mycron
 crontab mycron
 rm mycron
